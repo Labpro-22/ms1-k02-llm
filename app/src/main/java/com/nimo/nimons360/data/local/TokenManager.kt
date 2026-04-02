52c1cf749ac471ee5b0a18eb147b940d9a480395
AryoBama <13523088@std.stei.itb.ac.id>
2026-04-02 16:23:43 +0700

    feat: add token manager in local

---
+++ app/src/main/java/com/nimo/nimons360/data/local/TokenManager.kt
@@ -0,0 +1,34 @@
+package com.nimo.nimons360.data.local
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.core.content.edit
+
+class TokenManager(context: Context) {
+    private val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val sharedPreferences = EncryptedSharedPreferences.create(
+        context,
+        "secure_prefs",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    fun saveToken(token: String) {
+        sharedPreferences.edit{
+            putString("auth_token", token)
+        }
+    }
+
+    fun getToken(): String? = sharedPreferences.getString("auth_token", null)
+
+    fun clearToken() {
+        sharedPreferences.edit {
+            remove("auth_token")
+        }
+    }
+}

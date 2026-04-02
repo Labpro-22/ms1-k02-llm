92a80b23969bbedef5623f47d5a9df8c454bd50d
AryoBama <13523088@std.stei.itb.ac.id>
2026-04-02 08:09:08 +0700

    Initial Commit

---
+++ app/src/main/java/com/nimo/nimons360/core/utils/Contants.kt
@@ -0,0 +1,6 @@
+object Constants {
+    const val BASE_URL = "https://mad.labpro.hmif.dev"
+    const val WS_URL = "https://mad.labpro.hmif.dev/ws/live"
+    const val PREF_NAME = "nimons_pref"
+    const val AUTH_TOKEN = "auth_token"
+}
